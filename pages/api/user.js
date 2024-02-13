4649971bb00f5752178c8dbab5d779e603a529bc
Gabriel Manor <g.e.manor@gmail.com>
2024-02-13 21:32:19 +0200

    API Routes

---
+++ pages/api/user.js
@@ -0,0 +1,16 @@
+import { authorize, listUsers } from "../../lib/authorizer";
+
+export default async function handler(req, res) {
+    const user = req.headers["x-user"];
+
+    const allowed = await authorize(user, "list", "users");
+    if (!allowed) {
+        return res.status(403).json({
+            error: `User ${user} is not allowed to list users`,
+        });
+    }
+
+    const users = await listUsers();
+
+    return res.status(200).json(users);
+}
